acc39591350763400ecf50bbc0398cb093396173
react ninja <aadhar54@gmail.com>
2020-07-13 18:44:33 +0530

    port changed

--- index.js
+++ index.js
@@ -1,48 +1,49 @@
 //dependencies required for the app
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 //render css files
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 //placeholders for added task
-var task = ["buy socks", "practise with nodejs"];
+var task = ['buy socks', 'practise with nodejs'];
 //placeholders for removed task
-var complete = ["finish jquery"];
+var complete = ['finish jquery'];
 
-//post route for adding new task 
-app.post("/addtask", function(req, res) {
-    var newTask = req.body.newtask;
-    //add the new task from the post route
-    task.push(newTask);
-    res.redirect("/");
+//post route for adding new task
+app.post('/addtask', function (req, res) {
+  var newTask = req.body.newtask;
+  //add the new task from the post route
+  task.push(newTask);
+  res.redirect('/');
 });
 
-app.post("/removetask", function(req, res) {
-    var completeTask = req.body.check;
-    //check for the "typeof" the different completed task, then add into the complete task
-    if (typeof completeTask === "string") {
-        complete.push(completeTask);
-        //check if the completed task already exits in the task when checked, then remove it
-        task.splice(task.indexOf(completeTask), 1);
-    } else if (typeof completeTask === "object") {
-        for (var i = 0; i < completeTask.length; i++) {
-            complete.push(completeTask[i]);
-            task.splice(task.indexOf(completeTask[i]), 1);
-        }
+app.post('/removetask', function (req, res) {
+  var completeTask = req.body.check;
+  //check for the "typeof" the different completed task, then add into the complete task
+  if (typeof completeTask === 'string') {
+    complete.push(completeTask);
+    //check if the completed task already exits in the task when checked, then remove it
+    task.splice(task.indexOf(completeTask), 1);
+  } else if (typeof completeTask === 'object') {
+    for (var i = 0; i < completeTask.length; i++) {
+      complete.push(completeTask[i]);
+      task.splice(task.indexOf(completeTask[i]), 1);
     }
-    res.redirect("/");
+  }
+  res.redirect('/');
 });
 
 //render the ejs and display added task, completed task
-app.get("/", function(req, res) {
-    res.render("index", { task: task, complete: complete });
+app.get('/', function (req, res) {
+  res.render('index', { task: task, complete: complete });
 });
 
 //set app to listen on port 3000
-app.listen(3000, function() {
-    console.log("server is running on port 3000");
+app.listen(PORT, function () {
+  console.log('server is running on port 3000');
 });
